023af37ce8371fe8232e7ba8d6af2c027d845905
William van Doorn <wptmdoorn@gmail.com>
2021-04-15 09:24:17 +0200

    Preliminary support for multiple audio backends

--- pubmedtoaudio/audio.py
+++ pubmedtoaudio/audio.py
@@ -1,13 +1,21 @@
 import pdfplumber
 from io import BytesIO
-from gtts import gTTS
 import os
 
-def pdf_to_audio(text: list, out_name: str):       
-    tts = gTTS(text="\n".join(text), lang='en')
-    tts.save(out_name)     
+def pdf_to_audio(text: list, 
+                 out_name: str,
+                 backend: str = 'gtts'): 
     
-    #import pyttsx3
-    #engine = pyttsx3.init()
-    #engine.save_to_file("\n".join(text) , out_name)
-    #engine.runAndWait()       
+    if backend not in ['gtts', 'ttsx3']:
+        raise TypeError("Please provide a valid argument for the backend argument.")
+        return None
+          
+    if backend == 'gtts':
+        from gtts import gTTS
+        tts = gTTS(text="\n".join(text), lang='en')
+        tts.save(out_name)
+    elif backend == 'ttsx3':    
+        import pyttsx3
+        engine = pyttsx3.init()
+        engine.save_to_file("\n".join(text) , out_name)
+        engine.runAndWait()       
